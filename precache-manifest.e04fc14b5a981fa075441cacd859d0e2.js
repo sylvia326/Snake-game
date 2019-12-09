fe5543100ce12d9ecdb25359b8824edf5fda1708
sylvia326 <hxiaowei@uwo.ca>
2019-12-09 10:10:31 -0500

    Updates

---
+++ precache-manifest.e04fc14b5a981fa075441cacd859d0e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296b6427e5f714af748ba667fe42a108",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "17ef9c96097924815fc1",
+    "url": "/snake-game/static/css/main.b21b17e5.chunk.css"
+  },
+  {
+    "revision": "e1657d083b6ba8270a4f",
+    "url": "/snake-game/static/js/2.e02bf2e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/snake-game/static/js/2.e02bf2e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17ef9c96097924815fc1",
+    "url": "/snake-game/static/js/main.62d0021f.chunk.js"
+  },
+  {
+    "revision": "54bb296a58f66df810af",
+    "url": "/snake-game/static/js/runtime-main.e4fde404.js"
+  }
+]);
