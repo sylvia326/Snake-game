53986a043fd926fc777ff33de264310a6346d392
sylvia326 <hxiaowei@uwo.ca>
2019-12-10 01:00:57 -0500

    add test cases

---
+++ MochaJS-Test/test-case.js
@@ -0,0 +1,53 @@
+
+//
+
+
+const assert = require('chai').assert;
+const app = require('../App');
+
+
+  describe(‘App’, function(){
+    describe (' onKeyDown'', function(){
+      it(‘Press keyboard ‘up’, snake should turn up', function(){
+         };
+      it(‘Press keyboard ‘down’, snake should turn down', function(){
+        };
+      it(‘Press keyboard ‘left’, snake should turn left', function(){
+        };
+      it(‘Press keyboard ‘right’, snake should turn right', function(){
+        };
+
+    describe ('moveSnake', function(){
+      it('Snake should move at the beginning', function (){
+        };
+      it('Snake should move during the game', function (){
+        };
+      it('Snake should stop once game over’, function (){
+        };
+
+     describe ('checkIfOutOfBorder', function(){
+       it('Game over if snake out of right borders, function (){
+         };
+       it('Game over if snake out of left borders, function (){
+         };
+       it('Game over if snake out of bottom borders, function (){
+         };
+       it('Game over if snake out of top borders, function (){
+        };
+
+describe ('checkIfCollapsed', function(){
+    it(‘Game over if snake collapsed on its head’, function (){
+    };
+    it(‘Game over if snake collapsed on its body’, function (){
+    };
+
+    describe ('checkIfEat', function(){
+        it('Snake length should enlarge',function (){
+        };
+        it(There should be 10% increase of snake’s speed', function (){
+    };
+
+    describe ('onGameOver', function(){
+        it(‘Game over, and alert box should popup saying "gameover",function (){
+        };
+    });
